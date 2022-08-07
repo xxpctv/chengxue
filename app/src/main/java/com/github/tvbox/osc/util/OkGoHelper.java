2d92353443aa295fde8c3a850de5e52098a11dda
chengxue2020 <65702849+chengxue2020@users.noreply.github.com>
2022-08-07 21:35:57 +0800

    添加 DOH

--- app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
@@ -74,7 +74,19 @@ public class OkGoHelper {
                 return "https://doh.360.cn/dns-query";
             }
             case 4: {
+                return "https://dns.google/dns-query";
+            }
+            case 5: {
                 return "https://1.1.1.1/dns-query";   // takagen99
+            }
+            case 6: {
+                return "https://dns.adguard.com/dns-query";
+            }
+            case 7: {
+                return "https://dns.adguard.com/dns-query";
+            }
+            case 8: {
+                return "https://dns.quad9.net/dns-query";
             }
         }
         return "";
@@ -85,7 +97,11 @@ public class OkGoHelper {
         dnsHttpsList.add("腾讯");
         dnsHttpsList.add("阿里");
         dnsHttpsList.add("360");
+        dnsHttpsList.add("谷歌");
         dnsHttpsList.add("CloudFlare");
+        dnsHttpsList.add("AdGuard");
+        dnsHttpsList.add("AdGuard 家庭版");
+        dnsHttpsList.add("Quad9");
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor("OkExoPlayer");
         if (Hawk.get(HawkConfig.DEBUG_OPEN, false)) {
