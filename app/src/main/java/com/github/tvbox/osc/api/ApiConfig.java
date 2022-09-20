566a6e93694de8b630a27ecaa29b514fdf112d82
chengxue2020 <65702849+chengxue2020@users.noreply.github.com>
2022-09-20 23:32:26 +0800

    更新接口(接口加速)

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -80,7 +80,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://raw.githubusercontent.com/chengxue2020/Cat-ports/main/main.json");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://ghproxy.com/https://raw.githubusercontent.com/chengxue2020/Cat-ports/main/main.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
