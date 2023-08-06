e9d498739c1c2f48e91a51e474148cb5cc21cf47
chengxue2020 <65702849+chengxue2020@users.noreply.github.com>
2023-08-06 22:13:44 +0800

    更改升级按钮位置

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -63,13 +63,12 @@ public class App extends MultiDexApplication {
         // Hawk
         Hawk.init(this).build();
         Hawk.put(HawkConfig.DEBUG_OPEN, false);
-		Hawk.put(HawkConfig.HOME_REC, 1);      				// Home Rec 0=豆瓣, 1=站点推荐, 2=历史
+		Hawk.put(HawkConfig.HOME_REC, 1);      		// Home Rec 0=豆瓣, 1=站点推荐, 2=历史
 		Hawk.put(HawkConfig.HOME_REC_STYLE, true);	// 0=首页单行(左右切换)，1=首页多行(上下切换)
-		Hawk.put(HawkConfig.SEARCH_VIEW, 1);    			// 0=文字搜索列表 1=缩略图搜索列表
-		Hawk.put(HawkConfig.IJK_CODEC, "硬解码");    			// 硬解码
+		Hawk.put(HawkConfig.SEARCH_VIEW, 1);    	// 0=文字搜索列表 1=缩略图搜索列表
+		Hawk.put(HawkConfig.IJK_CODEC, "硬解码");   // 硬解码
         if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
             Hawk.put(HawkConfig.PLAY_TYPE, 1);
-			
         }
     }
 
