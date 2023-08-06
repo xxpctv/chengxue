4814aa8a1dd2341a114f67a8b8526ac0519bfcbc
chengxue2020 <65702849+chengxue2020@users.noreply.github.com>
2023-08-06 23:50:08 +0800

    添加 python 支持

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -18,6 +18,7 @@ import com.github.tvbox.osc.util.PlayerHelper;
 import com.github.tvbox.osc.util.js.JSEngine;
 import com.kingja.loadsir.core.LoadSir;
 import com.orhanobut.hawk.Hawk;
+import com.undcover.freedom.pyramid.PythonLoader;
 import com.p2p.P2PClass;
 
 import me.jessyan.autosize.AutoSizeConfig;
@@ -56,7 +57,10 @@ public class App extends MultiDexApplication {
                 .setSupportSubunits(Subunits.MM);
         PlayerHelper.init();
         JSEngine.getInstance().create();
-        FileUtils.cleanPlayerCache();
+        // Add Pyramid support
+        PythonLoader.getInstance().setApplication(this);
+
+		FileUtils.cleanPlayerCache();
     }
 
     private void initParams() {
