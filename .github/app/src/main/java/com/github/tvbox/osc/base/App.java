fc703dccd5fe386f50404662ed1fcb8f5c5efe0d
chengxue2020 <65702849+chengxue2020@users.noreply.github.com>
2023-08-06 23:28:47 +0800

    revert

---
+++ .github/app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -0,0 +1,110 @@
+package com.github.tvbox.osc.base;
+
+import android.app.Activity;
+import androidx.multidex.MultiDexApplication;
+
+import com.github.tvbox.osc.bean.VodInfo;
+import com.github.tvbox.osc.callback.EmptyCallback;
+import com.github.tvbox.osc.callback.LoadingCallback;
+import com.github.tvbox.osc.data.AppDataManager;
+import com.github.tvbox.osc.server.ControlManager;
+import com.github.tvbox.osc.util.AppManager;
+import com.github.tvbox.osc.util.EpgUtil;
+import com.github.tvbox.osc.util.FileUtils;
+import com.github.tvbox.osc.util.HawkConfig;
+import com.github.tvbox.osc.util.LOG;
+import com.github.tvbox.osc.util.OkGoHelper;
+import com.github.tvbox.osc.util.PlayerHelper;
+import com.github.tvbox.osc.util.js.JSEngine;
+import com.kingja.loadsir.core.LoadSir;
+import com.orhanobut.hawk.Hawk;
+import com.p2p.P2PClass;
+
+import me.jessyan.autosize.AutoSizeConfig;
+import me.jessyan.autosize.unit.Subunits;
+
+/**
+ * @author pj567
+ * @date :2020/12/17
+ * @description:
+ */
+public class App extends MultiDexApplication {
+    private static App instance;
+
+    private static P2PClass p;
+    public static String burl;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        initParams();
+        // OKGo
+        OkGoHelper.init(); //台标获取
+        EpgUtil.init();
+        // 初始化Web服务器
+        ControlManager.init(this);
+        //初始化数据库
+        AppDataManager.init();
+        LoadSir.beginBuilder()
+                .addCallback(new EmptyCallback())
+                .addCallback(new LoadingCallback())
+                .commit();
+        AutoSizeConfig.getInstance().setCustomFragment(true).getUnitsManager()
+                .setSupportDP(false)
+                .setSupportSP(false)
+                .setSupportSubunits(Subunits.MM);
+        PlayerHelper.init();
+        JSEngine.getInstance().create();
+        FileUtils.cleanPlayerCache();
+    }
+
+    private void initParams() {
+        // Hawk
+        Hawk.init(this).build();
+        Hawk.put(HawkConfig.DEBUG_OPEN, false);
+		Hawk.put(HawkConfig.HOME_REC, 1);      				// Home Rec 0=豆瓣, 1=站点推荐, 2=历史
+		Hawk.put(HawkConfig.HOME_REC_STYLE, true);	// 0=首页单行(左右切换)，1=首页多行(上下切换)
+		Hawk.put(HawkConfig.SEARCH_VIEW, 1);    			// 0=文字搜索列表 1=缩略图搜索列表
+		Hawk.put(HawkConfig.IJK_CODEC, "硬解码");    			// 硬解码
+        if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
+            Hawk.put(HawkConfig.PLAY_TYPE, 1);
+			
+        }
+    }
+
+    public static App getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        JSEngine.getInstance().destroy();
+    }
+
+
+    private VodInfo vodInfo;
+    public void setVodInfo(VodInfo vodinfo){
+        this.vodInfo = vodinfo;
+    }
+    public VodInfo getVodInfo(){
+        return this.vodInfo;
+    }
+
+    public static P2PClass getp2p() {
+        try {
+            if (p == null) {
+                p = new P2PClass(instance.getExternalCacheDir().getAbsolutePath());
+            }
+            return p;
+        } catch (Exception e) {
+            LOG.e(e.toString());
+            return null;
+        }
+    }
+
+    public Activity getCurrentActivity() {
+        return AppManager.getInstance().currentActivity();
+    }
+}
