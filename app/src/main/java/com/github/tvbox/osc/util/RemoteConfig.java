b07cf03b6910b14a18cd642cc474966c4054932d
chengxue2020 <65702849+chengxue2020@users.noreply.github.com>
2023-08-21 22:58:27 +0800

    0

--- app/src/main/java/com/github/tvbox/osc/util/RemoteConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/RemoteConfig.java
@@ -27,9 +27,9 @@ public class RemoteConfig {
     public static void Init(Context mContext){
         RemoteConfig.mContext = mContext;
         if (ToolUtils.isApkInDebug(mContext)){
-            remoteUrl = "https://chengxue2020.github.io/Cat-ports/config/remote_debug.ini";           
+            remoteUrl = "https://chengxue2020.github.io/Cat-ports/config/remote.txt";           
         }else{
-            remoteUrl = "https://chengxue2020.github.io/Cat-ports/config/remote.txt";
+            remoteUrl = "https://raw.githubusercontent.com/chengxue2020/Cat-ports/main/config/remote.txt";
         }
         LOG.e("RemoteConfig",
                 ToolUtils.isApkInDebug(mContext) ? "当前处于【调试】模式":"当前处于【正式】模式",
